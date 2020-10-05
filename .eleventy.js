16e967694506992e8fa72177d4fa909fb49fdacc
Nisha Chatwani <nchatwan@usc.edu>
2020-10-05 11:28:45 -0700

    404 page

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,28 @@
 const yaml = require('js-yaml');
+const fs = require("fs");
 const eleventyNavigationPlugin = require('@11ty/eleventy-navigation');
 const {
   DateTime
 } = require("luxon");
 
 module.exports = function(eleventyConfig) {
+
+  eleventyConfig.setBrowserSyncConfig({
+    callbacks: {
+      ready: function(err, bs) {
+
+        bs.addMiddleware("*", (req, res) => {
+          const content_404 = fs.readFileSync('dist/404.html');
+          // Provides the 404 content without redirect.
+          res.write(content_404);
+          // Add 404 http status code in request header.
+          // res.writeHead(404, { "Content-Type": "text/html" });
+          res.writeHead(404);
+          res.end();
+        });
+      }
+    }
+  });
 
   eleventyConfig.addPlugin(eleventyNavigationPlugin);
   
