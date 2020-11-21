5d4cd3e99f8d175d607fc9bb144d4deb46dd1054
Kumar Abhishek <manustays@users.noreply.github.com>
2020-11-21 23:42:38 +0530

    Merged "minify netlify js" and other updates

---
+++ src/_includes/js/google_analytics.js
@@ -0,0 +1,8 @@
+function googleAnalytics(key)
+{
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', key);
+}
