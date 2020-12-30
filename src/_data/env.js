a410c73ab11c42c0f7d2463506cf3244f46398d9
Kumar Abhishek <manustays@users.noreply.github.com>
2020-12-30 21:45:24 +0530

    Added product-wise dynamic form

--- src/_data/env.js
+++ src/_data/env.js
@@ -7,9 +7,17 @@
 
 module.exports = {
 	BASE_URL: process.env.BASE_URL || '',
+
 	GA_KEY: process.env.GA_KEY || '',
 	MS_CLARITY_TAG: process.env.MS_CLARITY_TAG || '',
-	ZOHO_SALESIQ_WIDGETCODE: process.env.ZOHO_SALESIQ_WIDGETCODE || '',
+
+	ZOHO_SALESIQ: process.env.ZOHO_SALESIQ || '',
+	ZOHO_SALESIQ_RETAIL: process.env.ZOHO_SALESIQ_RETAIL || '',
+	ZOHO_SALESIQ_API: process.env.ZOHO_SALESIQ_API || '',
+
 	ZOHO_FORM_URL: process.env.ZOHO_FORM_URL || '',
+	ZOHO_FORM_RETAIL_URL: process.env.ZOHO_FORM_RETAIL_URL || '',
+	ZOHO_FORM_API_URL: process.env.ZOHO_FORM_API_URL || '',
+
 	DEBUG_SECTIONS: process.env.DEBUG_SECTIONS || null
 }
