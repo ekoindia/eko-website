388cbde62b70049fdfc78ff784ad97e870238853
Kumar Abhishek <manustays@users.noreply.github.com>
2021-01-04 17:26:41 +0530

    Updated comments

--- utils/shortcodes.js
+++ utils/shortcodes.js
@@ -2,25 +2,36 @@ const outdent = require('outdent');
 
 module.exports = {
 
-	// IconSprite svg icon
-    // icon: () => name => `<svg class="icon icon--${name}" role="img" aria-hidden="true" width="24" height="24"><use xlink:href="#icon-${name}"></use></svg>`,
+	/**
+	 * IconSprite svg icon
+	 *
+	icon: () => name => `<svg class="icon icon--${name}" role="img" aria-hidden="true" width="24" height="24"><use xlink:href="#icon-${name}"></use></svg>`,
+	*/
 
-	// The scrolling mouse icon used under hero banners
+	/**
+	 * The scrolling mouse icon used under hero banners
+	 */
 	iconScroll: () => () => `<center><div class="icon-scroll"><div class="mouse"></div></div></center>`,
 
-	// A short text with colored dot on the left.
-	// Pass "filled" in the second 'classes' parameter to get rounded tag-like background.
+	/**
+	 * A short text with colored dot on the left.
+	 * Pass "filled" in the second 'classes' parameter to get rounded tag-like background.
+	 */
 	dotTag: () => (text, classes='') => `<span class="dottagbox ${classes}">${text}</span>`,
 
-	// Inline svg icons using the 'svgContents' filter
-	// Pass only the name (without the .svg extension) of
-	// the icon from the 'src/assets/ico' folder
+	/**
+	 * Inline svg icons using the 'svgContents' filter
+	 * Pass only the name (without the .svg extension) of
+	 *   the icon from the 'src/assets/ico' folder
+	 */
 	svgico: (config) => (name, classes = '') => config.getFilter("svgContents")(`/src/assets/ico/${name}.svg`, `svgico ${classes}`).replace(/<title>[^<]+<\/title>/i, ''),
 
 
-	// A link with an arrow-head (next-icon) to the right.
-	// Uses 'svgico' shortcode for inline SVG icon.
-	// Pass "bold" in the second 'classes' parameter for bold text.
+	/**
+	 * A link with an arrow-head (next-icon) to the right.
+	 * Uses 'svgico' shortcode for inline SVG icon.
+	 * Pass "bold" in the second 'classes' parameter for bold text.
+	 */
 	linkArrow: (config) => (url, text, classes = '') => {
 		if (!text) {
 			return '';
@@ -31,7 +42,9 @@ module.exports = {
 	},
 
 
-	// Return the current year (Used in the footer for copyright message)
-	thisYear: (config) => () => new Date().getFullYear().toString(),
+	/**
+	 * Return the current year (Used in the footer for copyright message)
+	 */
+	thisYear: () => () => new Date().getFullYear().toString(),
 
 }
