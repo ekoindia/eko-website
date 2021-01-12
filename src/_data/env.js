dd2039a813deb1bd59c630fe9cbc6039fcc3ab5c
Kumar Abhishek <manustays@users.noreply.github.com>
2021-01-12 20:20:06 +0530

    Updated visual debug style

--- src/_data/env.js
+++ src/_data/env.js
@@ -12,8 +12,7 @@ module.exports = {
 	MS_CLARITY_TAG: process.env.MS_CLARITY_TAG || '',
 
 	ZOHO_SALESIQ: process.env.ZOHO_SALESIQ || '',
-	ZOHO_SALESIQ_RETAIL: process.env.ZOHO_SALESIQ_RETAIL || '',
-	ZOHO_SALESIQ_API: process.env.ZOHO_SALESIQ_API || '',
+	ZOHO_PAGESENSE_URL: process.env.ZOHO_PAGESENSE_URL || '',
 
 	ZOHO_FORM_URL: process.env.ZOHO_FORM_URL || '',
 	ZOHO_FORM_RETAIL_URL: process.env.ZOHO_FORM_RETAIL_URL || '',
